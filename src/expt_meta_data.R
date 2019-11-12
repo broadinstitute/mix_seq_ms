d45d63aa0ff524d20938320b0e950927d3431479
James McFarland <jmmcfarl@broadinstitute.org>
2019-11-12 17:00:37 -0500

    update to new Taiga files with DepMap IDs

--- src/expt_meta_data.R
+++ src/expt_meta_data.R
@@ -4,55 +4,55 @@ sc_expts <- list(
     Trametinib_6hr_expt1 = list(
       expt_name = 'Trametinib_6hr_expt1',
       taiga_name = 'trametinib_6hr_expt1',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt1'
     ),
     Trametinib_24hr_expt1 = list(
       expt_name = 'Trametinib_24hr_expt1',
       taiga_name = 'trametinib_24hr_expt1',
-      taiga_version = 2,
+      taiga_version = 4,
       expt_batch = 'expt1'
     ),
     Bortezomib_6hr_expt1 = list(
       expt_name = 'Bortezomib_6hr_expt1',
       taiga_name = 'bortezomib_6hr_expt1',
-      taiga_version = 2,
+      taiga_version = 4,
       expt_batch = 'expt1'
     ),
     Bortezomib_24hr_expt1 = list(
       expt_name = 'Bortezomib_24hr_expt1',
       taiga_name = 'bortezomib_24hr_expt1',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt1'
     ),
     Idasanutlin_6hr_expt1 = list(
       expt_name = 'Idasanutlin_6hr_expt1',
       taiga_name = 'idasanutlin_6hr_expt1',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt1'
     ),
     Idasanutlin_24hr_expt1 = list(
       expt_name = 'Idasanutlin_24hr_expt1',
       taiga_name = 'idasanutlin_24hr_expt1',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt1'
     ),
     DMSO_24hr_expt1 = list(
       expt_name = 'DMSO_24hr_expt1',
       taiga_name = 'dmso_24hr_expt1',
-      taiga_version = 4,
+      taiga_version = 5,
       expt_batch = 'expt1'
     ),
     DMSO_6hr_expt1 = list(
       expt_name = 'DMSO_6hr_expt1',
       taiga_name = 'dmso_6hr_expt1',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt1'
     ),
     Untreated_6hr_expt1 = list(
       expt_name = 'Untreated_6hr_expt1',
       taiga_name = 'untreated_6hr_expt1',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt1'
     ),
   
@@ -60,55 +60,43 @@ sc_expts <- list(
     Trametinib_24hr_expt3 = list(
       expt_name = 'Trametinib_24hr_expt3',
       taiga_name = 'trametinib_24hr_expt3',
-      taiga_version = 4,
+      taiga_version = 5,
       expt_batch = 'expt3'
     ),
     Navitoclax_24hr_expt3 = list(
       expt_name = 'Navitoclax_24hr_expt3',
       taiga_name = 'navitoclax_24hr_expt3',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt3'
     ),
-    # Everolimus_6hr_expt3 = list(
-    #   expt_name = 'Everolimus_6hr_expt3',
-    #   taiga_name = 'everolimus_6hr_expt3',
-    #   taiga_version = 3,
-    #   expt_batch = 'expt3'
-    # ),
     Dabrafenib_24hr_expt3 = list(
       expt_name = 'Dabrafenib_24hr_expt3',
       taiga_name = 'dabrafenib_24hr_expt3',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt3'
     ),
     BRD3379_24hr_expt3 = list(
       expt_name = 'BRD3379_24hr_expt3',
       taiga_name = 'brd3379_24hr_expt3',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt3'
     ),
     BRD3379_6hr_expt3 = list(
       expt_name = 'BRD3379_6hr_expt3',
       taiga_name = 'brd3379_6hr_expt3',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt3'
     ),
-    # CGS15943_6hr_expt3 = list(
-    #   expt_name = 'CGS15943_6hr_expt3',
-    #   taiga_name = 'cgs15943_6hr_expt3',
-    #   taiga_version = 3,
-    #   expt_batch = 'expt3'
-    # ),
     DMSO_6hr_expt3 = list(
       expt_name = 'DMSO_6hr_expt3',
       taiga_name = 'dmso_6hr_expt3',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt3'
     ),
     DMSO_24hr_expt3 = list(
       expt_name = 'DMSO_24hr_expt3',
       taiga_name = 'dmso_24hr_expt3',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt3'
     ),
   
@@ -116,25 +104,25 @@ sc_expts <- list(
     sgGPX4_1_expt2 = list(
       expt_name = 'sgGPX4_1_expt2',
       taiga_name = 'sggpx4_1_expt2',
-      taiga_version = 2,
+      taiga_version = 3,
       expt_batch = 'expt2'
     ),
     sgGPX4_2_expt2 = list(
       expt_name = 'sgGPX4_2_expt2',
       taiga_name = 'sggpx4_2_expt2',
-      taiga_version = 2,
+      taiga_version = 3,
       expt_batch = 'expt2'
     ),
     sgOR2J2_expt2 = list(
       expt_name = 'sgOR2J2_expt2',
       taiga_name = 'sgor2j2_expt2',
-      taiga_version = 2,
+      taiga_version = 3,
       expt_batch = 'expt2'
     ),
     sgLACZ_expt2 = list(
       expt_name = 'sgLACZ_expt2',
       taiga_name = 'sglacz_expt2',
-      taiga_version = 2,
+      taiga_version = 3,
       expt_batch = 'expt2'
     ),
   
@@ -142,111 +130,66 @@ sc_expts <- list(
     tramet_tc_expt5 = list(
       expt_name = 'trametinib_tc_expt5',
       taiga_name = 'trametinib_tc_expt5',
-      taiga_version = 3,
+      taiga_version = 4,
       expt_batch = 'expt5'
     ),
   
-#EXPT6
-    MDM2_expt6 = list(
-      expt_name = 'MDM2_expt6',
-      taiga_name = 'mdm2_expt6',
-      taiga_version = 2,
-      expt_batch = 'expt6'
-    ),
-
-#EXPT9
-  # untreated_G029442_GFP_24hr_1_expt9 = list(
-  #   # data_sets = list(expt = 'untreated_G029442_GFP_24hr_1'),
-  #   expt_name = 'untreated_G029442_GFP_24hr_1_expt9',
-  #   taiga_name = NULL,
-  #   taiga_version = NULL,
-  #   expt_batch = 'expt9'
-  # ),
-  # untreated_G029442_GFP_24hr_2_expt9 = list(
-  #   # data_sets = list(expt = 'untreated_G029442_GFP_24hr_2'),
-  #   expt_name = 'untreated_G029442_GFP_24hr_2_expt9',
-  #   taiga_name = NULL,
-  #   taiga_version = NULL,
-  #   expt_batch = 'expt9'
-  # ),
 
 #EXPT10
   DMSO_expt10 = list(
     expt_name = 'DMSO_expt10',
     taiga_name = 'dmso_expt10',
-    taiga_version = 5,
+    taiga_version = 6,
     expt_batch = 'expt10'
   ),
   Everolimus_expt10 = list(
     expt_name = 'Everolimus_expt10',
     taiga_name = 'everolimus_expt10',
-    taiga_version = 5,
+    taiga_version = 6,
     expt_batch = 'expt10'
   ),
   Afatinib_expt10 = list(
     expt_name = 'Afatinib_expt10',
     taiga_name = 'afatinib_expt10',
-    taiga_version = 5,
+    taiga_version = 6,
     expt_batch = 'expt10'
   ),
   Taselisib_expt10 = list(
     expt_name = 'Taselisib_expt10',
     taiga_name = 'taselisib_expt10',
-    taiga_version = 5,
+    taiga_version = 6,
     expt_batch = 'expt10'
   ),
   AZD5591_expt10 = list(
     expt_name = 'AZD5591_expt10',
     taiga_name = 'azd5591_expt10',
-    taiga_version = 5,
+    taiga_version = 6,
     expt_batch = 'expt10'
   ),
   JQ1_expt10 = list(
     expt_name = 'JQ1_expt10',
     taiga_name = 'jq1_expt10',
-    taiga_version = 5,
+    taiga_version = 6,
     expt_batch = 'expt10'
   ),
-  # THZ21021_expt10 = list(
-  #   expt_name = 'THZ21021_expt10',
-  #   taiga_name = 'thz21021_expt10',
-  #   taiga_version = 5,
-  #   expt_batch = 'expt10'
-  # ),
   Gemcitabine_expt10 = list(
     expt_name = 'Gemcitabine_expt10',
     taiga_name = 'gemcitabine_expt10',
-    taiga_version = 5,
+    taiga_version = 6,
     expt_batch = 'expt10'
   ),
   Trametinib_expt10 = list(
     expt_name = 'Trametinib_expt10',
     taiga_name = 'trametinib_expt10',
-    taiga_version = 5,
+    taiga_version = 6,
     expt_batch = 'expt10'
   ),
   Prexasertib_expt10 = list(
     expt_name = 'Prexasertib_expt10',
     taiga_name = 'prexasertib_expt10',
-    taiga_version = 6,
+    taiga_version = 7,
     expt_batch = 'expt10'
   )
-
-#EXPT11
-  # XPR1_PAX8_expt11 = list(
-  #   # data_sets = list(expt = 'combined'),
-  #   expt_name = 'XPR1_PAX8_expt11',
-  #   taiga_name = NULL,
-  #   taiga_version = NULL,
-  #   expt_batch = 'expt11'
-  # ),
-  # combined_expt11 = list(
-  #   # data_sets = list(expt = 'combined'),
-  #   expt_name = 'combined_expt11',
-  #   taiga_name = NULL,
-  #   taiga_version = NULL,
-  #   expt_batch = 'expt11'
-  # )
 )
 
 
@@ -419,15 +362,6 @@ sc_DE_meta <- list(
       ),
       load_from_taiga = TRUE
     ),
-    # CGS15943_6hr_expt3 = list(
-    #   drug_name = 'CGS-15943',
-    #   data_sets = list(control_1 = 'DMSO_24hr_expt3',
-    #                    control_2 = 'DMSO_6hr_expt3',
-    #                    treat_1 = 'CGS15943_6hr_expt3'),
-    #   expt_batch = 'expt3',
-    #   expt_name = 'CGS15943_6hr_expt3',
-    #   load_from_taiga = TRUE
-    # ),
     everolimus_6hr_expt3 = list(
       drug_name = 'everolimus',
       data_sets = list(control_1 = 'DMSO_24hr_expt3',
@@ -542,15 +476,6 @@ sc_DE_meta <- list(
       annotate_omics = NULL,
       load_from_taiga = TRUE
     ),
-    # THZ21021_expt10 = list(
-    #   data_sets = list(control = 'DMSO_expt10',
-    #                    treat = 'THZ21021_expt10'),
-    #   drug_name = 'THZ-2-102-1',
-    #   expt_batch = 'expt10',
-    #   expt_name = 'THZ21021_expt10',
-    #   annotate_omics = NULL,
-    #   load_from_taiga = TRUE
-    # ),
     Gemcitabine_expt10 = list(
       data_sets = list(control = 'DMSO_expt10',
                        treat = 'Gemcitabine_expt10'),
